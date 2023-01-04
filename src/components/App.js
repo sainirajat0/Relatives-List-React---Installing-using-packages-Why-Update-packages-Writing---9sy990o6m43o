b28003377cef32956cee8773d29861a65a5c6505
sainirajat0 <rajatsaini005@gmail.com>
2023-01-04 17:02:46 +0530

    Update app.js and index.js file

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+            let relatives = ["bhanu", "prashansha", "rashi"];
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+              <ol key="relativeList">
+                {relatives.map((relative, index) => (
+                <li key={`relativeListItem${index + 1}`}>{relative}</li>
+                ))}
+               </ol>
             </div>
         )
     }
